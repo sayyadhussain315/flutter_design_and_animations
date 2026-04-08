a32e66bfdac60257a3c2fba29579cc8ff0e35f24
sayyadhussain315 <sayyadhussain315@gmail.com>
2026-04-08 22:54:00 +0500

    20260408

---
+++ Themes.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    const appName = 'GeeksForGeeks';
+
+    return MaterialApp(
+      title: appName,
+      theme: ThemeData(
+        floatingActionButtonTheme:
+        FloatingActionButtonThemeData(
+          backgroundColor: Colors.red,
+        ),
+        appBarTheme: AppBarTheme(foregroundColor: Colors.white),
+        brightness: Brightness.light,
+        primaryColor: Colors.green,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.green,
+          secondary: Colors.deepOrangeAccent,
+        ),
+        fontFamily: 'Georgia',
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          titleLarge:   TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyMedium:   TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: appName),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      body: Center(
+        child: Container(
+          color: Theme.of(context).colorScheme.secondary,
+          child: Text(
+            'Hello Geeks!',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.arrow_circle_up),
+      ),
+    );
+  }
+}
