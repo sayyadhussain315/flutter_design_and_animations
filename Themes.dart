55fec5680540dd75206b6039aeea985646c0d67d
sayyadhussain315 <sayyadhussain315@gmail.com>
2026-04-08 22:56:41 +0500

    20260408

--- Themes.dart
+++ Themes.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    const appName = 'GeeksForGeeks';
+    const appName = 'My App';
 
     return MaterialApp(
       title: appName,
@@ -34,6 +37,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatelessWidget {
   final String title;
 
